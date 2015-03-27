4f5f0b84161df81faa25646fa0dc970ec1b5750e
Mark Chance <mark@peopledesigned.com>
2015-03-27 08:52:39 -0600

    add colorization plugin

--- ansible/roles/jenkins/files/internal_scripts/makeappjobs.py
+++ ansible/roles/jenkins/files/internal_scripts/makeappjobs.py
@@ -28,9 +28,9 @@ import sys, os, httplib
 #  - $PomPath
 #  - $GroupId
 #  - $ArtifactId
-# 
+#  - $CodeBranch (optional)
 
-def fixupFile(filename, config_xml):
+def fixupFile(filename, config_xml, branch):
     #
     # fixup job template
     #
@@ -50,7 +50,8 @@ def fixupFile(filename, config_xml):
         GitRepoPath = GitRepoPath,
         PomPath = PomPath,
         ArtifactoryUrl = ArtifactoryUrl,
-        PomPathOnly = PomPathOnly
+        PomPathOnly = PomPathOnly,
+        CodeBranch = branch
     )
 
     jenkins_url = None
@@ -175,4 +176,4 @@ if __name__ == '__main__':
             xml=myfile.read()
         if filename.endswith("-"+ApplicationFlavor+".xml"):
             filename = filename.replace("-"+ApplicationFlavor, '')
-        fixupFile(filename, xml)
+        fixupFile(filename, xml, branch)
